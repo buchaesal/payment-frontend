256a0c6e6a4db0022b4d763563ad47cdf3467751
danbi <danbi07@plateer.com>
2024-03-27 16:46:05 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title> Vue & Nuxt Mastery Class </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <NuxtPage />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup lang="ts">
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+</script>
