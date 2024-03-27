021940bf1af58ebe6a0214fbff7a4ad2fde3d1d3
Choi Danbi <eks4116@gmail.com>
2024-03-27 22:10:12 +0900

    Course 데이터 가져오기

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  $END$
+  <q-card class="my-card" @click="$emit('click')">
+    <img :src="thumbnail" />
+
+    <q-card-section>
+      <div class="text-h6 ellipsis">{{ title }}</div>
+      <div class="text-subtitle2 ellipsis text-grey-8">
+        {{ subtitle }}
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+interface Props {
+  thumbnail: string;
+  title: string;
+  subtitle: string;
+}
+withDefaults(defineProps<Props>(), {
+  thumbnail: '',
+  title: '',
+  subtitle: '',
+});
 
-</style>
+defineEmits<{
+  click: [];
+}>();
+</script>
