8a76ff428f50f6472f62c3fccb5e90d31ae58266
danbi <danbi07@plateer.com>
2024-06-12 18:58:13 +0900

    fix

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -2,4 +2,4 @@ import type { CourseReturn } from '~/types/course';
 import useFetchWithCache from '~/composables/useFetchWithCache';
 
 export const useCourse = (courseSlug: string): Promise<Maybe<CourseReturn>> =>
-  useFetchWithCache(`api/course/${courseSlug}`);
+  useFetchWithCache(`api/courses/${courseSlug}`);
