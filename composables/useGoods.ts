057156cd36d5da6ff5cf29b32176108d851a7437
danbi <danbi07@plateer.com>
2024-10-16 15:41:49 +0900

    fix

--- composables/useGoods.ts
+++ composables/useGoods.ts
@@ -2,10 +2,12 @@ import type {Goods} from "~/types/goods";
 import goodsList from '~/composables/goodsData';
 
 export const useGoods = (): Goods[] => {
-    return goodsList.map((goods, index) => {
-        return {
-            ...goods,
-            name: `${goods.name}${index}`
-        }
-    });
+    goodsList.forEach((goods, index) => goods.name = `${goods.name}${index}`);
+    return goodsList;
+    // return goodsList.map((goods, index) => {
+    //     return {
+    //         ...goods,
+    //         name: `${goods.name}${index}`
+    //     }
+    // });
 }
